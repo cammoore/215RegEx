ae342643aee71fc4bd6460cdfe2061ac1ff70c6b
Cam Moore <cammymoore07@gmail.com>
2016-05-16 11:16:47 -1000

    what is going on. ESLint isn't working

---
+++ app/imports/startup/client/routes.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Carleton on 4/27/2016.
+ */
---
+++ app/imports/startup/server/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Carleton on 4/27/2016.
+ */
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server startup.
+});
